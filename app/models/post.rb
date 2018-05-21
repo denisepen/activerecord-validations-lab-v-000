7ba9cf2b05573edb29be5d5202f4a713b1f83730
Denise Pendleton <dpendl1@hotmail.com>
2018-05-21 01:47:41 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,11 +10,12 @@ end
 
 class Post < ActiveRecord::Base
   include ActiveModel::Validations
-  validates_with MyValidator
+
   validates :title, presence: true
   validates :content, length: { minimum: 150 }
  validates :summary, length: { minimum: 5 }
    validates :summary, length: { maximum: 150 }
    validates_exclusion_of :category, :in => ["Bowling Ball"]
-   
+   validates_with MyValidator
+
 end
