8f3c13c90b5857fe345eccee3ede7b2dc0f01569
Denise Pendleton <dpendl1@hotmail.com>
2018-05-21 01:06:52 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: { minimum: 150 }
-  validates :summary, length: { minimum: 100 }
+  validates :summary, length: { minimum: 400 }
   validates :summary, length: { maximum: 1000 }
 end
