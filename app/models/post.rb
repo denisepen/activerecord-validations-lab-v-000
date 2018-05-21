94230c42d8f05fa73715fce105f72c614fe7369f
Denise Pendleton <dpendl1@hotmail.com>
2018-05-21 01:28:29 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,5 @@ class Post < ActiveRecord::Base
  validates :summary, length: { minimum: 5 }
    validates :summary, length: { maximum: 150 }
    validates_exclusion_of :category, :in => ["Bowling Ball"]
+   validates_inclusion_of :category, :in => ["clickbait"]
 end
