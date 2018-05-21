891cadd11fde29fb1bc8caca2a2ad3d371f818be
Denise Pendleton <dpendl1@hotmail.com>
2018-05-21 01:51:02 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class MyValidator < ActiveModel::Validator
   def validate(post)
     if post.title.starts_with? 'True Facts' || post.title == nil
-      post.errors[:title] << 'Must be clickbait'
+      post.errors[:title] << 'Error'
     end
   end
 end
@@ -10,7 +10,7 @@ end
 
 class Post < ActiveRecord::Base
   include ActiveModel::Validations
-   
+
   validates :title, presence: true
   validates :content, length: { minimum: 150 }
  validates :summary, length: { minimum: 5 }
